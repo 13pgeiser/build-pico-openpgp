4e25315d61be8f81ee700d476926ca32040c789e
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2025-02-01 16:00:12 +0100

    Create source archive before compiling

--- docker_build.sh
+++ docker_build.sh
@@ -5,10 +5,10 @@ cat ./scripts/Dockerfile.bookworm | docker build -t pico_opengpg -
 docker run -d --name pico_opengpg pico_opengpg sleep 86400 # 24 hours...
 docker cp ./build.sh pico_opengpg:/build.sh
 docker exec -t pico_opengpg bash /build.sh pico checkout
+docker exec -t pico_opengpg bash /build.sh pico archive_sources
 docker exec -t pico_opengpg bash /build.sh pico build_release
 docker exec -t pico_opengpg bash /build.sh pico2 build_release
 docker exec -t pico_opengpg bash /build.sh pimoroni_tiny2350 build_release
-docker exec -t pico_opengpg bash /build.sh pico archive_sources
 docker cp pico_opengpg:/release ./
 docker rm -f pico_opengpg || true
 docker system prune -f -a
